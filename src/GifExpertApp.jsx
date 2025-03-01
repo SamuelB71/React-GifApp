940b2a3367a0b0309880119aa80d2c622461d2c0
Samuel <bellolozadasamuel@gmail.com>
2025-03-01 12:34:11 -0300

    docs actualizado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,7 @@ import { AddCategory, GifGrid } from "./components";
 
 const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState([ "Hoshimachi Suisei", ]);
+    const [categories, setCategories] = useState([ "", ]);
 
     const onAddCategory = (newCategory) =>{
         if(categories.includes(newCategory)) return
